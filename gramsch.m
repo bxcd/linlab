d307ca16853b33ae7ac1ee147813f55543ebc803
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-01-24 00:14:20 -0500

    Update working file

--- gramsch.m
+++ gramsch.m
@@ -1,22 +1,27 @@
-function[U] = gramsch(V)
-    
-    [m, n] = size(V);
-    
-    U = zeros(m,n);
-    P = zeros(m,0);
-    
-    for i = (1 : n)
-        [~, b] = size(P); 
-        u = zeros(m, 1);
-        p = zeros(m, 1);
-        for j = (0 : b)
-            if j > 0
-                p = (p + (V(:, i)' * U(:, j)) * U(:, j));
-            end
+function[Q] = gramsch(V)
+
+  V = [v1 v2 v3]
+
+  [m, n] = size(V)
+
+  Q = zeros(m,n)
+  R = zeros(m,n)
+  P = zeros(m,0)
+
+  for i = (1 : n)
+    [~, b] = size(P)
+    q = zeros(m, 1)
+    r = zeros(m, 1)
+    p = zeros(m, 1)
+    for j = (0 : b)
+        if j > 0
+            R(j, i) = (V(:, i)' * Q(:, j))
+            p = (p + (R(j, i) * Q(:, j)))
         end
-        u = (V(:, i) - p)/norm(V(:, i) - p);
-        U(:, i) = u;
-        P(:, i) = p;
-        disp(P)
     end
+    P(:, i) = p
+    R(i, i) = norm(V(:, i) - p)
+    q = (V(:, i) - p)/R(i, i)
+    Q(:, i) = q
+  end
 end
