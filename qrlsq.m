9780613d5d6b11b59556fe63d7bd66a29f3e637b
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-01-24 16:19:43 -0500

    Update working files

--- qrlsq.m
+++ qrlsq.m
@@ -1,8 +1,21 @@
 % QR least-squares solution
-% finds d for {Q, R} obtained from QR factorization of Ax = b
+% for an overdetermined system, finds x for which (QR)x most closely approximates b
+% as well as d between (QR)x and b
 
-function x = qrlsq(Q, R, b)
+function [x, d1] = qrlsq(Q, R, b)
 
-    x = rref([R (Q'*b)]);
+    X = rref([R (Q'*b)])
+    [~, n] = size(b)
+    [~, q] = size(X)
+    x = X(:, (q - n + 1):q)
+
+    A = Q*R
+    P = A*((A'*A)^(-1))*A'
+
+    p1 = A*x
+    p2 = P*b
+
+    d1 = norm(b - p1)
+    d2 = norm(b - p2)
 
 end
